b422764ded875a020b460d636c8a0563f7229c16
nalaso <alans.sunny@gmail.com>
2024-08-27 13:38:52 +0530

    feat: Add type casting for JsxParser components

--- src/components/preview-screen.tsx
+++ src/components/preview-screen.tsx
@@ -1,18 +1,22 @@
 import * as UI from '@/components/ui';
 import JsxParser from 'react-jsx-parser'
+import React, { ComponentType, ExoticComponent } from 'react';
+
+type JsxParserComponents = Record<string, ComponentType<any> | ExoticComponent<any>>;
+
+function castComponents(components: typeof UI): JsxParserComponents {
+  return components as unknown as JsxParserComponents;
+}
 
 const PreviewScreen = ({ html_code }: { html_code: string }) => {
-
   return (
-    // <div className="w-full h-full bg-white rounded-lg  shadow-lg p-2 border">
     <div className='relative'>
-
       <JsxParser
-        components={{ ...UI }}
+        components={castComponents(UI)}
         jsx={html_code}
-        />
-        </div>
-    // </div>
+      />
+    </div>
   );
 };
+
 export default PreviewScreen; 
