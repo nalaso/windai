75495f0ef2eedc5d0d306cb63f14383c04751b3d
Alan Sunny <73838186+nalaso@users.noreply.github.com>
2024-09-07 10:34:45 +0530

    chore: Increase number of UIs fetched in getUIHome to 11

--- src/actions/ui/get-uis/index.ts
+++ src/actions/ui/get-uis/index.ts
@@ -86,7 +86,7 @@ export const getUI = async (UIId: string) => {
 
 export const getUIHome = async () => {
     const uis = await db.uI.findMany({
-        take: 8,
+        take: 11,
         orderBy: { createdAt: 'desc' },
         include: {
             user: {
