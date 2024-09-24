88b317d55a01a5abfdc7defb70200f7518cfca28
Alan Sunny <73838186+nalaso@users.noreply.github.com>
2024-09-24 08:37:16 +0530

    Dep: vertex models

--- src/lib/supportedllm.ts
+++ src/lib/supportedllm.ts
@@ -1,4 +1,4 @@
-export const supportedllm = ['githubMistral','githubOpenAI:gpt-4o', 'mistral', 'groq', 'cohere', 'vertex', 'google']
+export const supportedllm = ['githubMistral','githubOpenAI:gpt-4o', 'mistral', 'groq', 'cohere', 'google']
 
 export const isModelSupported = (modelId: string) => {
     return supportedllm.some(llm => modelId.toLowerCase().startsWith(llm.toLowerCase()));
